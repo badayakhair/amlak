da0079f26dd50f216f149894df156151fd1dbddc
badayakhair <it@badayakhair.sa>
2026-06-05 17:22:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // رابط Google Apps Script Web App
-window.AMLAK_API_URL = 'https://script.google.com/macros/s/AKfycbyxyZEhy4lQXL4-0ZUnlUU55U13akbb9XFPP_pJ50bGhXfAq4bJsPM0jlqx1CSXGRro_g/exec';
+window.AMLAK_API_URL = 'https://script.google.com/macros/s/AKfycbxUXrl9eN0YoBUKRW3inAxtrqbx_ld6jyjw9Nhj3ynh_b1iM9q2pZiZIJnZb431JpQ8_g/exec';
